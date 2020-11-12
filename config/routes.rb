820567c93ed8f0b21cedabfaf0880e8c938a2b63
Stéphane ZE-OGIER <contact@ze-ogier.com>
2020-11-12 22:24:30 +0100

    Put tagline below product title

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
+Rails.application.routes.draw do
+  devise_for :users
+  root to: 'products#index' # And remove the generated route `products/index`
+end
+
 # Rails.application.routes.draw do
 #   get 'products/index'
 #   devise_for :users
 #   root to: 'pages#home'
 #   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 # end
-Rails.application.routes.draw do
-  devise_for :users
-  root to: 'products#index' # And remove the generated route `products/index`
-end
