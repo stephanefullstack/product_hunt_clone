041da16e95a007ac7f35ce7f7b07d7c65260cb01
Stéphane ZE-OGIER <contact@ze-ogier.com>
2020-11-13 01:25:47 +0100

    remove test for coming features

--- test/system/products_test.rb
+++ test/system/products_test.rb
@@ -6,20 +6,20 @@ class ProductsTest < ApplicationSystemTestCase
     assert_selector "h2", text: "Log in"
   end
 
-  test "lets a signed in user create a new product" do
-    login_as users(:george)
-    visit "/products/new"
-    # save_and_open_screenshot
+  # test "lets a signed in user create a new product" do
+  #   login_as users(:george)
+  #   visit "/products/new"
+  #   # save_and_open_screenshot
 
-    fill_in "product_name", with: "Le Wagon"
-    fill_in "product_tagline", with: "Change your life: Learn to code"
-    # save_and_open_screenshot
+  #   fill_in "product_name", with: "Le Wagon"
+  #   fill_in "product_tagline", with: "Change your life: Learn to code"
+  #   # save_and_open_screenshot
 
-    click_on 'Create Product'
-    # save_and_open_screenshot
+  #   click_on 'Create Product'
+  #   # save_and_open_screenshot
 
-    # Should be redirected to Home with new product
-    assert_equal root_path, page.current_path
-    assert_text "Change your life: Learn to code"
-  end
+  #   # Should be redirected to Home with new product
+  #   assert_equal root_path, page.current_path
+  #   assert_text "Change your life: Learn to code"
+  # end
 end
