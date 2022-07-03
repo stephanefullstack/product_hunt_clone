4bec26bf4fd1eae3a071cce5c16af356ce8fbc1d
stephane <contact@ze-ogier.com>
2022-07-03 22:53:28 +0200

    tests passed

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,4 +4,16 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all
   end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new
+    @product.name = params[:product][:product_name]
+    @product.tagline = params[:product][:tagline]
+    @product.save
+    redirect_to root_url
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'products#index'
+
+  resources :products, only: [:index, :new, :create] 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
